7472589317d80d3d773c5f0f3dd1f3f6a787b876
iki1uc <entertrainer@web.de>
2026-07-23 15:32:12 +0200

    Create EFFECT.js

---
+++ EFFECT.js
@@ -0,0 +1,11 @@
+window.EFFECT = {
+    start(){
+        return {
+            pulse: "Pulse aktiv",
+            wave: "Wave aktiv",
+            cube: "Cube aktiv",
+            hyper: "Hyper aktiv",
+            stamp: Date.now()
+        };
+    }
+};
