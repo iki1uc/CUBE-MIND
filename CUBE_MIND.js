0a8856c2356bedfb02675ef591ec86f8d9d98665
iki1uc <entertrainer@web.de>
2026-07-23 15:33:38 +0200

    Create CUBE_MIND.js

---
+++ CUBE_MIND.js
@@ -0,0 +1,9 @@
+window.CUBE_MIND = {
+    start(){
+        return {
+            matrix: MATRIX9x9.build(),
+            mode: "KI-Modus aktiv",
+            stamp: Date.now()
+        };
+    }
+};
