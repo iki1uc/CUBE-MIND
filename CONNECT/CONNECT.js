1bdf22f41fb8b1a76f316686bee48bc96953bc47
iki1uc <entertrainer@web.de>
2026-07-27 00:03:01 +0200

    Update CONNECT.js

--- CONNECT/CONNECT.js
+++ CONNECT/CONNECT.js
@@ -1,1 +1,18 @@
+export const CONNECT = {
+  lan: true,
+  wlan: true,
+  gate: "WLAN-GATE",
+  wloch: "WLOCH-ROUTE",
+  allxall: "ALLXALL-CONNECT",
+
+  link(room, pipe) {
+    return {
+      room,
+      pipe,
+      via: "LAN/WLAN",
+      route: this.wloch,
+      mode: this.allxall
+    };
+  }
+};
 
